05186b9ce150b2054d453e796ecf788ef757ca17
Mateusz Daszkiewicz <mateusz@wordshop.co.uk>
2024-06-15 15:00:20 +0100

    Display dialogue & resize camera

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,45 @@
-// utility scripts to handle dialogues & camera movement
-
-export function test() {
-    console.log("test");
+export function setCamScale(k) {
+    // resize camera scale based on screen size
+    const resizeFactor = k.width() / k.height();
+    if (resizeFactor < 1) {
+      k.camScale(k.vec2(1));
+    } else {
+      k.camScale(k.vec2(1.5));
+    }
 }
+export function displayDialogue(text, onDisplayEnd) {
+    // render dialogue text
+    const dialogueUI = document.querySelector(".textbox-container");
+    const dialogue = document.querySelector(".dialogue");
+
+    dialogueUI.style.display = "block";
+    let index = 0;
+    let currentText = "";
+    const intervalRef = setInterval(() => {
+      if (index < text.length) {
+        currentText += text[index];
+        dialogue.innerHTML = currentText;
+        index++;
+        return;
+      }
+      clearInterval(intervalRef);
+    }, 1);
+
+    function onCloseBtnClick() {
+      onDisplayEnd();
+      dialogueUI.style.display = "none";
+      dialogue.innerHTML = "";
+      clearInterval(intervalRef);
+      closeBtn.removeEventListener("click", onCloseBtnClick);
+    }
+
+    const closeBtn = document.querySelector("ui-close-btn");
+
+    closeBtn.addEventListener("click", onCloseBtnClick);
+
+    addEventListener("keypress", (key) => {
+      if (key.code === "Enter") {
+        onCloseBtnClick();
+      }
+    });
+  }
